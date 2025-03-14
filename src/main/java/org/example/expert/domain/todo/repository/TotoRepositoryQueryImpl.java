0547f2dfd3546e9417fbf8a4ee0cc2abddb35930
Heni <Heni0717>
2025-03-14 15:59:48 +0900

    lv2 (8) QueryDSL
    - queryDSL 프레임워크를 사용하기 위한 설정
    	-> build.gradle 의존성 추가
    	-> configuration에 entitymanager를 주입받은
    	JPAQueryFactory 빈으로 등록
    - queryDSL을 사용할 repository 및 impl 생성 후 적용
    - chore: gradle 재설치

---
+++ src/main/java/org/example/expert/domain/todo/repository/TotoRepositoryQueryImpl.java
@@ -0,0 +1,26 @@
+package org.example.expert.domain.todo.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.example.expert.domain.todo.entity.Todo;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+import static org.example.expert.domain.todo.entity.QTodo.todo;
+
+@Repository
+@RequiredArgsConstructor
+public class TotoRepositoryQueryImpl implements TodoRepositoryQuery {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public Optional<Todo> findByIdWithUser(Long todoId) {
+        return Optional.ofNullable(jpaQueryFactory.selectFrom(todo)
+                .where(todo.id.eq(todoId))
+                .leftJoin(todo.user)
+                .fetchJoin()
+                .fetchOne());
+    }
+}
